520e84081f504157a2ab671437f845abdb0cf23e
Nahuel Raposo <nahuelraposo@gmail.com>
2023-07-22 12:16:08 -0300

    .

---
+++ app/src/test/java/com/example/myapplication/AffirmationsAdapterTests.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import android.content.Context
+import com.example.myapplication.adapter.ItemAdapter
+import com.example.myapplication.model.Affirmation
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class AffirmationsAdapterTests {
+
+    private val context = mock(Context::class.java)
+
+    @Test
+    fun adapter_size() {
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+        val adapter = ItemAdapter(context, data)
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
+    }
+}
